18a825c3b88528d37e2e21cae736b60c39a3db77
Maleeha Bhuiyan <maleeha.bhuiyan@baruchmail.cuny.edu>
2020-05-12 10:44:14 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,3 +1,12 @@
 def find_max_value(array)
-  # Add your solution here
+  counter = 0
+  max_value = 0
+  while array[counter] do
+    array[counter] == max_value
+    if  array[counter] == max_value
+      return max_value
+    else
+      count += 1
+    end
+  end
 end
